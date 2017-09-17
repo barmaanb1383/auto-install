b2754edcc5ee3388001f0d03eee3e71bbda68fa2
barmaan <30550912+barmaanb1383@users.noreply.github.com>
2017-09-17 13:01:43 -0400

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,35 @@
+cd 
+mkdir Git
+
+cd Git
+
+git clone https://github.com/barmaanb1383/brut3k1t.git
+
+git clone https://github.com/ex0dus-0x/pypayload
+
+apt-get install vega 
+
+git clone https://github.com/crypt0b0y/V3n0M-Scanner.git
+
+git clone https://github.com/crypt0b0y/wifite2.git
+
+git clone https://github.com/ChrisTruncer/PenTestScripts.git
+
+git clone https://github.com/Veil-Framework/Veil.git
+
+cd brut3k1t
+
+./installer
+
+pip install -r requirements.txt
+
+cd ..
+
+cd V3n0M-Scanner/
+
+python3 setup.py install --user
+
+cd ..
+cd Veil 
+cd setup 
+./setup -c 
